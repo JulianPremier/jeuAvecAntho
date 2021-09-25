5655ed7a1b809197c09876f74232182d84524bc6
BuildTools <75986500+Akamya@users.noreply.github.com>
2021-09-26 00:01:53 +0200

    Server and Client communicating well with each over
    Server and Client works just fine, made some tweaks to the Engine

--- moteur.py
+++ moteur.py
@@ -1,11 +1,15 @@
-import sys, pygame
+import sys
+import pygame
 import socket
 import os
 
 
 class Engine:
-    def __init__(self):
+    def __init__(self, e_sock):
         pygame.init()
+        self.e_sock = e_sock
+        e_sock.debug()
+        e_sock.connect_to_server()
 
         size = 500, 400
         self.window = pygame.display.set_mode(size)
@@ -20,6 +24,7 @@ class Engine:
     def refresh(self):
         pygame.draw.rect(self.window, (20, 228, 214), self.rect)
         pygame.display.flip()
+        self.e_sock.send_data(str(self.pos_x) + ', ' + str(self.pos_y))
 
     def topMove(self):
         self.window.fill(pygame.Color(0, 0, 0))
@@ -39,7 +44,7 @@ class Engine:
         self.pos_x = self.pos_x - 10
         self.refresh()
 
-    def rigthMove(self):
+    def rightMove(self):
         self.window.fill(pygame.Color(0, 0, 0))
         self.rect = self.rect.move(10, 0)
         self.pos_x = self.pos_x + 10
@@ -58,9 +63,10 @@ class Engine:
                     elif pygame.key.get_pressed()[pygame.K_q]:
                         self.leftMove()
                     elif pygame.key.get_pressed()[pygame.K_d]:
-                        self.rigthMove()
+                        self.rightMove()
                     os.system("cls")
                     print("posX = ", self.pos_x, ";posY = ", self.pos_y)
+
 
 class GameSocket:
     host = ''
@@ -83,15 +89,20 @@ class GameSocket:
             print('Connected to server.')
         except ConnectionRefusedError or ConnectionError:
             print('weird')
-        finally:
-            self.sock.close()
+
+    def send_data(self, data):
+        data = data.encode('utf8')
+        print(data)
+        self.sock.sendall(data)
+
+    def close_socket(self):
+        self.sock.close()
 
 
 if __name__ == '__main__':
     socket = GameSocket()
-    socket.debug()
-    socket.connect_to_server()
-    engine = Engine()
+    engine = Engine(socket)
     engine.listenKeyboardEvent()
+
 # INTERESSANT
 # a = pygame.key.get_pressed()[pygame.K_a]
--- server.py
+++ server.py
@@ -2,15 +2,22 @@ import socket
 
 host, port = ('', 4977)
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.bind((host, port))
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((host, port))
+    s.listen(5)
+    conn, addr = s.accept()
+    with conn:
+        print('Client connected', addr)
+        while True:
+            data = conn.recv(1024)
+            data = data.decode('utf8')
+            print(data)
+        if not data:
+            conn.close()
+            s.close()
 
-while True:
-    sock.listen(5)
-    conn, addr = sock.accept()
-    print('Client connected')
 
-conn.close()
-sock.close()
+
+
 
 
