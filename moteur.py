f83570319b6da71d8a5be75c7d9a92d9100f1fd0
Julian <51192787+JulianPremier@users.noreply.github.com>
2021-09-25 23:04:03 +0200

    Add files via upload

--- moteur.py
+++ moteur.py
@@ -1,51 +1,67 @@
 import sys, pygame
 import os
 
-class moteur:
-    def run():
+class engine:
+    def __init__(self):
         pygame.init()
 
         size = 500, 400
-        window = pygame.display.set_mode(size)
+        self.window = pygame.display.set_mode(size)
 
-        rect_pos = pos_x, pos_y = 250, 200
-        rect = pygame.Rect(pos_x, pos_y, 25, 20)
-        pygame.draw.rect(window, (20, 228, 214), rect)
+        self.pos_x = 250
+        self.pos_y = 200
+        self.rect = pygame.Rect(self.pos_x, self.pos_y, 25, 20)
+
+        pygame.draw.rect(self.window, (20, 228, 214), self.rect)
         pygame.display.flip()
 
+    def refresh(self):
+        pygame.draw.rect(self.window, (20, 228, 214), self.rect)
+        pygame.display.flip()
+
+    def topMove(self):
+        self.window.fill(pygame.Color(0, 0, 0))
+        self.rect = self.rect.move(0, -10)
+        self.pos_y = self.pos_y - 10
+        self.refresh()
+
+    def bottomMove(self):
+        self.window.fill(pygame.Color(0, 0, 0))
+        self.rect = self.rect.move(0, 10)
+        self.pos_y = self.pos_y + 10
+        self.refresh()
+
+    def leftMove(self):
+        self.window.fill(pygame.Color(0, 0, 0))
+        self.rect = self.rect.move(-10, 0)
+        self.pos_x = self.pos_x - 10
+        self.refresh()
+
+    def rigthMove(self):
+        self.window.fill(pygame.Color(0, 0, 0))
+        self.rect = self.rect.move(10, 0)
+        self.pos_x = self.pos_x + 10
+        self.refresh()
+
+    def listenKeyboardEvent(self):
         while 1:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT: 
                     sys.exit()
                 elif event.type == pygame.KEYDOWN:
                     if pygame.key.get_pressed()[pygame.K_z]:
-                        window.fill(pygame.Color(0, 0, 0))
-                        rect = rect.move(0, -10)
-                        pos_y = pos_y - 10
-                        pygame.draw.rect(window, (20, 228, 214), rect)
-                        pygame.display.flip()
+                        self.topMove()
                     elif pygame.key.get_pressed()[pygame.K_s]:
-                        window.fill(pygame.Color(0, 0, 0))
-                        rect = rect.move(0, 10)
-                        pos_y = pos_y + 10
-                        pygame.draw.rect(window, (20, 228, 214), rect)
-                        pygame.display.flip()
+                        self.bottomMove()
                     elif pygame.key.get_pressed()[pygame.K_q]:
-                        window.fill(pygame.Color(0, 0, 0))
-                        rect = rect.move(-10, 0)
-                        pos_x = pos_x - 10
-                        pygame.draw.rect(window, (20, 228, 214), rect)
-                        pygame.display.flip()
+                        self.leftMove()
                     elif pygame.key.get_pressed()[pygame.K_d]:
-                        window.fill(pygame.Color(0, 0, 0))
-                        rect = rect.move(10, 0)
-                        pos_x = pos_x + 10
-                        pygame.draw.rect(window, (20, 228, 214), rect)
-                        pygame.display.flip()
+                        self.rigthMove()
                     os.system("cls")
-                    print("posX = ", pos_x, ";posY = ", pos_y)
+                    print("posX = ", self.pos_x, ";posY = ", self.pos_y)
 
-moteur.run()
+engine = engine()
+engine.listenKeyboardEvent()
     
 #INTERESSANT
 #a = pygame.key.get_pressed()[pygame.K_a]
